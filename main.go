08fe1dfdadbef2ae50089564e158caac236185f6
Vishal Sony <75028186+Vishal-Sony@users.noreply.github.com>
2024-02-22 12:20:24 +0530

    Update linting

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -97,7 +98,7 @@ func main() {
 	srv := &http.Server{
 		Addr:              ":" + port,
 		Handler:           router,
-		ReadHeaderTimeout: 100,
+		ReadHeaderTimeout: 10 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
